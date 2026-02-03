7dd34fcfdc8516b7c2aedfd021e82420817fbbb9
cleitonsilva14 <cleiton18ads@gmail.com>
2026-02-03 19:30:39 -0300

    Iniciando implementação de Orders

--- src/main/java/br/com/btgbackend/orderms/model/Order.java
+++ src/main/java/br/com/btgbackend/orderms/model/Order.java
@@ -1,4 +1,27 @@
 package br.com.btgbackend.orderms.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collation = "tb_order")
 public class Order {
+
+    @MongoId
+    private Long orderId;
+    private Long customerId;
+    private BigDecimal total;
+    private List<OrderItem> items;
+
 }
--- src/main/java/br/com/btgbackend/orderms/model/OrderItem.java
+++ src/main/java/br/com/btgbackend/orderms/model/OrderItem.java
@@ -1,4 +1,20 @@
 package br.com.btgbackend.orderms.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderItem {
+
+    private String product;
+    private Integer quantity;
+    private BigDecimal price;
+
 }
