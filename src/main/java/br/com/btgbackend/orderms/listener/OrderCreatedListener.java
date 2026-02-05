64f59dfdc49672b2cbb32664052b622dd23d254d
cleitonsilva14 <cleiton18ads@gmail.com>
2026-02-05 13:26:58 -0300

    Implementado orderService.save

--- src/main/java/br/com/btgbackend/orderms/listener/OrderCreatedListener.java
+++ src/main/java/br/com/btgbackend/orderms/listener/OrderCreatedListener.java
@@ -1,6 +1,8 @@
 package br.com.btgbackend.orderms.listener;
 
 import br.com.btgbackend.orderms.dto.OrderCreatedEvent;
+import br.com.btgbackend.orderms.service.OrderService;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -10,13 +12,18 @@ import org.springframework.stereotype.Component;
 import static br.com.btgbackend.orderms.configuration.RabbitMqConfiguration.ORDER_CREATED_QUEUE;
 
 @Component
+@RequiredArgsConstructor
 public class OrderCreatedListener {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    private final OrderService orderService;
+
     @RabbitListener(queues = ORDER_CREATED_QUEUE)
     public void listen(Message<OrderCreatedEvent> message) {
         logger.info("Message consumed: {}", message);
+
+        orderService.save(message.getPayload());
     }
 
 
